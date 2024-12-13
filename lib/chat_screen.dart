0bdfd1192ebabf252c352000f5eda6d4d1eead89
Ghost_1412 <aminsafri02@gmail.com>
2024-12-13 11:23:03 +0800

    test

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -56,7 +56,7 @@ class _ChatScreenState extends State<ChatScreen> {
       _showSessionEndedDialog();
       return;
     }
-
+    //test
     Map<String, dynamic> sessionData = sessionDoc.data() as Map<String, dynamic>;
     Map<String, dynamic> encryptedGroupKeys = Map<String, String>.from(sessionData['encryptedGroupKeys'] ?? {});
     keyVersion = sessionData['keyVersion'] ?? 1;
